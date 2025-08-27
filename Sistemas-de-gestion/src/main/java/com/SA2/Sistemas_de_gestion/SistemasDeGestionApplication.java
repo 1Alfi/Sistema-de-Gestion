5d3ef350f817f599287d237d9c7a5f12f5a0b5dc
1Alfi <alfanico173@gmail.com>
2025-08-27 00:55:11 -0300

    Agrega el código inicial del proyecto

---
+++ Sistemas-de-gestion/src/main/java/com/SA2/Sistemas_de_gestion/SistemasDeGestionApplication.java
@@ -0,0 +1,13 @@
+package com.SA2.Sistemas_de_gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasDeGestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasDeGestionApplication.class, args);
+	}
+
+}
---
+++ Sistemas-de-gestion/src/test/java/com/SA2/Sistemas_de_gestion/SistemasDeGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SA2.Sistemas_de_gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemasDeGestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
