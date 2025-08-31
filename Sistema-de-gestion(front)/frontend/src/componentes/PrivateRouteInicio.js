e760979c6ccb4caf721be8c08cbfcc8b0f20b526
1Alfi <alfanico173@gmail.com>
2025-08-31 20:23:34 -0300

    Correccion PrivateRoute

--- Sistema-de-gestion(front)/frontend/src/componentes/PrivateRouteInicio.js
+++ Sistema-de-gestion(front)/frontend/src/componentes/PrivateRouteInicio.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 //Recibe un componente como parámetro
-const PrivateRoute = ({ children }) => {
+const PrivateRoute = ({ children, requiredRole }) => {
   // Verificar si el token existe en el almacenamiento local
   const token = localStorage.getItem('token');
+  
+  const userRole = getUserRoleFromToken(token); 
 
   // Si no hay token, redirigir al login
   if (!token) {
     return <Navigate to="/login" />;
+  }
+
+  if (userRole === "ADMINISTRADOR") {
+    return children;
   }
 
   if (requiredRole && userRole !== requiredRole) {
